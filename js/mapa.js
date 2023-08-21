08f933360f4bd0b0979369ca391d96a0fc2e7186
humano264 <126574178+humano264@users.noreply.github.com>
2023-08-21 16:59:07 -0300

    Add files via upload

--- js/mapa.js
+++ js/mapa.js
@@ -1,12 +1,21 @@
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
 (function() {
   
-    var states = document.getElementsByClassName("estado")
-    
-    for(var i = 0; i < states.length; i++) {
-      states[i].onclick = function() {
-          alert(this.getAttribute('name') + ' ' + this.getAttribute('code'));
-      }
+  var states = document.getElementsByClassName("estado")
+  
+  for(var i = 0; i < states.length; i++) {
+    states[i].onclick = function() {
+        alert(this.getAttribute('name') + ' ' + this.getAttribute('code'));
     }
-    
-  })();
+  }
+  
+})();
+
 
